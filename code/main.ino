7c1f0ad530220f7ab8a4983820508d886ef29dcb
Sassa <145195245+nesyass@users.noreply.github.com>
2026-04-03 19:12:28 +0700

    Update main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,1 +1,34 @@
+#include "DHT.h"
 
+#define DHTPIN 4
+#define DHTTYPE DHT22   // pakai DHT22 untuk Wokwi
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("DHT Sensor Test");
+  dht.begin();
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  // cek error
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" °C  |  ");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  delay(2000);
+}
